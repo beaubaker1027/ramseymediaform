f96ba84b9fbc59a02919ed14d659a73a30bfda36
Beau Baker <beaubaker1027@gmail.com>
2019-08-13 10:10:12 -0500

    updated validation, styles, removed a theme, simplified scripting because html5 validation was working fine

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-const formName = "InfoForm";
-
-var validations = {
-  fullName: (text)=>{
-    document.getElementById('fullName-error')
-    return text
-  },
-  email: (text)=> {
-    return text;
-  },
-  phone: (text)=>{
-    return formatPhoneNumber(text);
-  },
-  city: (text)=>{
-    return text;
-  },
-  state: (text)=>{
-    return text;
-  },
-  zip: (text)=>{
-    return text;
-  }
-}
-
 function getElementId({id, ...event}){
   if(!id) return;
   return id;
@@ -32,27 +8,12 @@ function toggleClass(id, theme){
   return;
 }
 
+function changeInputValue(el, mutation){
+  let { value, id } = el;
+  let newValue = mutation(value);
+  el.value = newValue;
+}
+
 function formatPhoneNumber(phoneNumberString) {
-  console.log(phoneNumberString)
-  var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
-  var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
-  if (match) {
-    return '(' + match[1] + ') ' + match[2] + '-' + match[3]
-  }
-  return null
-}
-
-function validateForm(){
-  let inputs = Object.keys(validations).map(key=>{
-    return document.forms[formName][key];
-  })
-  console.log(inputs);
-  return false;
-}
-
-function validateInput({name,value, ...rest}){
-  if(!validations[name]){
-    return
-  }
-  validations[name](value);
+  return phoneNumberString.trim().replace(/\s/g,"");
 }
